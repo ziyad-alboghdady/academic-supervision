5b6a23b968dc378b93b7efaaeb31a4532c9927e2
ZiyadAlboghdady <ziyadalboghdady@gmail.com>
2026-03-13 11:31:06 +0300

    adding studies repository

--- src/main/java/org/example/academic_supervision/Repository/StudiesRepository.java
+++ src/main/java/org/example/academic_supervision/Repository/StudiesRepository.java
@@ -1,4 +1,9 @@
 package org.example.academic_supervision.Repository;
 
-public class StudiesRepository {
+import org.example.academic_supervision.Model.Studies;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudiesRepository extends JpaRepository<Studies, Long> {
 }
