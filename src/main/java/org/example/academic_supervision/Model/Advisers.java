56c1880f7905d7a545ef91af1e63c5b1ee244fb9
ZiyadAlboghdady <ziyadalboghdady@gmail.com>
2026-03-13 12:02:19 +0300

    adding the advisers model with annotations

--- src/main/java/org/example/academic_supervision/Model/Advisers.java
+++ src/main/java/org/example/academic_supervision/Model/Advisers.java
@@ -1,4 +1,36 @@
 package org.example.academic_supervision.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Advisers {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Adviser name cannot be empty")
+    @Size(min = 3, max = 50, message = "Adviser name must be between 3 and 50 characters")
+    @Column(length = 50, nullable = false)
+    private String adviserName;
+
+    @NotBlank(message = "Department cannot be empty")
+    @Size(min = 2, max = 50, message = "Department must be between 2 and 50 characters")
+    @Column(length = 50, nullable = false)
+    private String department;
+
+    @OneToMany(mappedBy = "adviser", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Supervises> supervises;
 }
