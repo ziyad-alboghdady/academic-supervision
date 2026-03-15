8e0a3ef127dc8cb9e3b556a33b16b41d8eafa3be
Uaekz <ardaumut1995@gmail.com>
2026-03-15 17:06:04 +0300

    Service for Adviser is done

--- src/main/java/org/example/academic_supervision/Repository/AdvisersRepository.java
+++ src/main/java/org/example/academic_supervision/Repository/AdvisersRepository.java
@@ -1,4 +1,9 @@
 package org.example.academic_supervision.Repository;
 
-public class AdvisersRepository {
+import org.example.academic_supervision.Model.Advisers;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdvisersRepository extends JpaRepository<Advisers, Long> {
 }
--- src/main/java/org/example/academic_supervision/Service/IAdvisersService.java
+++ src/main/java/org/example/academic_supervision/Service/IAdvisersService.java
@@ -1,4 +1,14 @@
 package org.example.academic_supervision.Service;
 
+import org.example.academic_supervision.DTO.AdvisersDTO;
+import org.example.academic_supervision.Model.Advisers;
+
+import java.util.List;
+
 public interface IAdvisersService {
+    List<AdvisersDTO> getAllAdvisers();
+    AdvisersDTO getAdviserById(Long id);
+    AdvisersDTO createAdviser(Advisers adviser);
+    AdvisersDTO updateAdviser(Long id, Advisers adviser);
+    void deleteAdviser(Long id);
 }
