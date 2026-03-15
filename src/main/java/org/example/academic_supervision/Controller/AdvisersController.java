62005b45f3c8b6d142a282305f95c1df9025f02f
Uaekz <ardaumut1995@gmail.com>
2026-03-15 17:17:27 +0300

    'get' mapping for AdviserController is added

--- src/main/java/org/example/academic_supervision/Controller/AdvisersController.java
+++ src/main/java/org/example/academic_supervision/Controller/AdvisersController.java
@@ -12,4 +12,11 @@ public class AdvisersController {
     public ResponseEntity<List<AdvisersDTO>> getAllAdvisers() {
         return new ResponseEntity<>(advisersService.getAllAdvisers(), HttpStatus.OK);
     }
+
+    @GetMapping(value = "/get/{id}", produces = "application/json")
+    public ResponseEntity<AdvisersDTO> getAdviser(@PathVariable Long id) {
+        if (id == null || id <= 0) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        logger.info("Get adviser by id {}", id);
+        return new ResponseEntity<>(advisersService.getAdviserById(id), HttpStatus.OK);
+    }
 }
