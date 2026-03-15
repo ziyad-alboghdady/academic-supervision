9d59f112dbaadfa876ffe8eaff239c1a2de5be78
Uaekz <ardaumut1995@gmail.com>
2026-03-15 17:15:26 +0300

    logger and service for AdviserController is added

--- src/main/java/org/example/academic_supervision/Controller/AdvisersController.java
+++ src/main/java/org/example/academic_supervision/Controller/AdvisersController.java
@@ -1,5 +1,7 @@
 package org.example.academic_supervision.Controller;
 
 public class AdvisersController {
+    private final static Logger logger = LoggerFactory.getLogger(AdvisersController.class);
+    private final IAdvisersService advisersService;
 
 }
