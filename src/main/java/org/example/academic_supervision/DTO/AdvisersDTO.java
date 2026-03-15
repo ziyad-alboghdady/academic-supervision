d0515f8b7d0a70a66d5b7f2df31185019f2f18d6
Uaekz <ardaumut1995@gmail.com>
2026-03-15 17:02:03 +0300

    DTO forAdviser is done

--- src/main/java/org/example/academic_supervision/DTO/AdvisersDTO.java
+++ src/main/java/org/example/academic_supervision/DTO/AdvisersDTO.java
@@ -1,4 +1,14 @@
 package org.example.academic_supervision.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AdvisersDTO {
+    private Long id;
+    private String name;
+    private String department;
 }
