02269c14ffc3cda50cf5135f79b615a02b95ac10
Uaekz <ardaumut1995@gmail.com>
2026-03-15 17:16:40 +0300

    constructor and 'all' mapping for AdviserController is added

--- src/main/java/org/example/academic_supervision/Controller/AdvisersController.java
+++ src/main/java/org/example/academic_supervision/Controller/AdvisersController.java
@@ -4,4 +4,12 @@ public class AdvisersController {
     private final static Logger logger = LoggerFactory.getLogger(AdvisersController.class);
     private final IAdvisersService advisersService;
 
+    public AdvisersController(IAdvisersService advisersService) {
+        this.advisersService = advisersService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<AdvisersDTO>> getAllAdvisers() {
+        return new ResponseEntity<>(advisersService.getAllAdvisers(), HttpStatus.OK);
+    }
 }
